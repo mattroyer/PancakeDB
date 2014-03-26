512bb7967f7602eaae098f6b56a9cc1e97d8aee0
Matt Royer <matthew.royer@gmail.com>
2014-03-26 08:12:11 -0600

    Added the file's current directory so the seed_data can be found easily.

--- example/test.rb
+++ example/test.rb
@@ -1,4 +1,4 @@
 require_relative "../pancakedb"
 
-myDb = PancakeDB.new "seed_data"
+myDb = PancakeDB.new "#{File.dirname(__FILE__)}/seed_data"
 p myDb.get_records("state", "ut").collect_elements("carl")
